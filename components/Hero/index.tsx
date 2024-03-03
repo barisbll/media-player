fc09f2264e611987687f859efae55ece6adee71e
Baris Balli <barisbll.dev@gmail.com>
2024-03-03 14:20:00 +0100

    cover added to the video

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -1,7 +1,14 @@
 export const Hero = () => {
   return (
-    <div className="w-full h-1/2scHeight">
-      <video className="w-full h-full object-cover" autoPlay loop muted>
+    <div className="w-full h-1/2scHeight relative flex">
+      <div className="w-full h-full bg-black bg-opacity-50 z-10"></div>
+      <video
+        className="w-full h-full object-cover absolute top-0 left-0 z-0"
+        autoPlay
+        loop
+        muted
+        controls
+      >
         <source src="/videos/bg-video.mp4" type="video/mp4" />
       </video>
     </div>
